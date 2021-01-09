6050401e53bf0ec8b187b22825b94f9dc0a3b3f0
nguyenxnguyen113 <65146661+nguyenxnguyen113@users.noreply.github.com>
2021-01-09 15:13:04 +0700

    --- Auto-Git Commit ---

---
+++ closure.js
@@ -0,0 +1,3 @@
+/*Q1:
+- A Closure is a function that is nested inside another function (a parent function) that can use the global variable, 
+the local variable of the parent function, and its own local variable (lexical scoping).*/
