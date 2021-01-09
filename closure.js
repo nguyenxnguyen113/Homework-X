4c1b00cb494429bd0e37ce13f7af76f8dabc5a1c
nguyenxnguyen113 <65146661+nguyenxnguyen113@users.noreply.github.com>
2021-01-09 15:43:14 +0700

    --- Auto-Git Commit ---

--- closure.js
+++ closure.js
@@ -1,3 +1,16 @@
-/*Q1:
-- A Closure is a function that is nested inside another function (a parent function) that can use the global variable, 
-the local variable of the parent function, and its own local variable (lexical scoping).*/
+/*
+- Q1: A Closure is a function that is nested inside another function (a parent function) that can use the global variable, 
+the local variable of the parent function, and its own local variable (lexical scoping).
+- Q2:
+*/
+
+let x = 2, fns = []
+
+function closure (x, arr) {
+    x = 5;
+    for(let i = 0; i < x; i++) {
+
+    }
+};
+
+console.log((x*2) === fns[x*2]
